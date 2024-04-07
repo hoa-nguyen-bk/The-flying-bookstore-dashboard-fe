443c7ce5ea7fbf18314a441133bd36cc66450411
student1060632fpt <hoa.nguyen_bk@hcmut.edu.vn>
2024-04-07 22:36:20 +0700

    Update dashboard names and descriptions, delete old code and my own dashboard web

--- src/views/components/AuthorsTable.vue
+++ src/views/components/AuthorsTable.vue
@@ -1,299 +1,58 @@
 <template>
   <div class="card mb-4">
     <div class="card-header pb-0">
-      <h6>Authors table</h6>
+      <h6>Listing table</h6>
     </div>
     <div class="card-body px-0 pt-0 pb-2">
       <div class="table-responsive p-0">
         <table class="table align-items-center mb-0">
           <thead>
             <tr>
-              <th
-                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-              >
-                Author
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Id
               </th>
-              <th
-                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
-              >
-                Function
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Quantity
               </th>
-              <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-              >
-                Status
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Address
               </th>
-              <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-              >
-                Employed
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Lease Rate
               </th>
-              <th class="text-secondary opacity-7"></th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Deposit Fee
+              </th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Penalty Rate
+              </th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                Description
+              </th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img1"
-                      size="sm"
-                      border-radius="lg"
-                      class="me-3"
-                      alt="user1"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">John Michael</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      john@creative-tim.com
-                    </p>
-                  </div>
-                </div>
+            <tr v-for="(listing, index) in listings" :key="index">
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.id }}</p>
               </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Organization</p>
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.quantity }}</p>
               </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.address }}</p>
               </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >23/04/18</span
-                >
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.leaseRate }}</p>
               </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.depositFee }}</p>
               </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img2"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user2"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Alexa Liras</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      alexa@creative-tim.com
-                    </p>
-                  </div>
-                </div>
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.penaltyRate }}</p>
               </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programator</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >11/01/19</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img3"
-                      size="sm"
-                      border-radius="lg"
-                      class="me-3"
-                      alt="user3"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Laurent Perrier</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      laurent@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Executive</p>
-                <p class="text-xs text-secondary mb-0">Projects</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >19/09/17</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img4"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user4"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Michael Levi</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      michael@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programator</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >24/12/08</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img5"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user5"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Richard Gran</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      richard@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Executive</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >04/10/21</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img6"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user6"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Miriam Eric</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      miriam@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programtor</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >14/09/20</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
+              <td  class="align-middle text-center">
+                <p class="text-xs font-weight-bold mb-0">{{ listing.description }}</p>
               </td>
             </tr>
           </tbody>
@@ -304,30 +63,39 @@
 </template>
 
 <script>
-import SoftAvatar from "@/components/SoftAvatar.vue";
-import SoftBadge from "@/components/SoftBadge.vue";
-import img1 from "../../assets/img/team-2.jpg";
-import img2 from "../../assets/img/team-3.jpg";
-import img3 from "../../assets/img/team-4.jpg";
-import img4 from "../../assets/img/team-3.jpg";
-import img5 from "../../assets/img/team-2.jpg";
-import img6 from "../../assets/img/team-4.jpg";
+import axios from 'axios';
 
 export default {
   name: "authors-table",
   data() {
     return {
-      img1,
-      img2,
-      img3,
-      img4,
-      img5,
-      img6,
+      listings: []
     };
   },
   components: {
-    SoftAvatar,
-    SoftBadge,
   },
+  mounted() {
+    this.fetchListings();
+  },
+  methods: {
+    fetchListings() {
+      axios.get('http://localhost:8082/api/listing')
+        .then(response => {
+          this.listings = response.data.map(listing => ({
+            id: listing.id,
+            quantity: listing.quantity,
+            address: listing.address,
+            expiryDate: listing.expiryDate,
+            leaseRate: listing.leaseRate,
+            depositFee: listing.depositFee,
+            penaltyRate: listing.penaltyRate,
+            description: listing.description.substring(0, 50),
+          }));
+        })
+        .catch(error => {
+          console.error('Error fetching listings:', error);
+        });
+    }
+  }
 };
 </script>
