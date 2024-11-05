4fc8654e7611522a55ccf42206262a01514abd1e
Hòa Nguyễn <48762609+student1060632fpt@users.noreply.github.com>
2024-11-05 14:54:55 +0700

    Refactor: Revert API URL change in env.js to use environment variable

--- src/store/env.js
+++ src/store/env.js
@@ -1,3 +1,3 @@
-// const port = process.env.VUE_APP_API_URL || "http://localhost:8080"
-const port = "http://localhost:8080" //TODO revert this
+const port = process.env.VUE_APP_API_URL || "http://localhost:8080"
+// const port = "http://localhost:8080" //TODO revert this
 export { port }
